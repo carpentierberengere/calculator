b136ee848b4b1a58ae5f8dedbaad2fd7fcaf3acf
Bérengère Carpentier <72983609+carpentierberengere@users.noreply.github.com>
2020-11-16 15:29:18 +0100

    rangement

--- calculator.js
+++ calculator.js
@@ -2,17 +2,26 @@ const main = document.querySelector("main");
 
 const screen = document.createElement("div");
 
-// style 
+//--------------style--------------------------// 
 main.appendChild(screen);
 screen.style.border = "2px solid #E920AF";
 screen.style.height = "50px";
 screen.style.width = "360px";
 
-// style 2
+//---------------style 2-----------------------//
 let myScreen = document.querySelector("div");
 myScreen.style.backgroundColor = "#F9AFE4";
+myScreen.style.backgroundPosition = "center";
 
-// numbers & buttons
+let myButton = document.querySelector("div");
+myButton.style.color = "#411BA6";
+
+//let myMain = document.querySelector("main");
+//myMain.style.backgroundColor = "#AB8DFA";
+//myMain.style.backgroundPosition = "center";
+
+
+//-------------------numbers-------------------// 
 for (let i = 0; i <= 9; i++) {
     const button = document.createElement("button");
     button.textContent = i;
@@ -24,6 +33,7 @@ for (let i = 0; i <= 9; i++) {
     });
 }
 
+//-----------------button CLEAR----------------//
 const clear = document.createElement("button");
 clear.textContent = "C";
 main.appendChild(clear);
@@ -31,29 +41,7 @@ clear.addEventListener("click", function(){
     screen.textContent = "";
 });
 
-//const egal = document.createElement("button");
-//egal.textContent = "=";
-//main.appendChild(egal);
-//egal.addEventListener("click", function egal(String){
-//    return Function('return' + String)()
-    //screen.textContent= Function('return' + screen.textContent)(); 
-    //eval(screen.textContent);
-//});
-
-const egal = document.createElement("button");
-egal.textContent = "=";
-main.appendChild(egal);
-egal.addEventListener("click", function (){
-    screen.textContent= eval(screen.textContent);
-});
-
-// Ajout de decimale 
-//const decimal = document.createElement("button");
-//decimal.textContent = ",";
-//main.appendChild(decimal);
-//decimal.addEventListener("click", function(){
-//    screen.textContent = 
-
+//-----------------buttons OPERATION-------------//
 const operations= ["+","-","*",":"]
 
 for (let i=0; i<= operations.length-1;i++) {
@@ -64,6 +52,41 @@ for (let i=0; i<= operations.length-1;i++) {
         screen.textContent += operations[i];
     });
 }
+
+//------------------button EGAL-----------------//
+const egal = document.createElement("button");
+egal.textContent = "=";
+main.appendChild(egal);
+egal.addEventListener("click", function (){
+    screen.textContent= eval(screen.textContent);
+});
+
+
+
+//const egal = document.createElement("button");
+//egal.textContent = "=";
+//main.appendChild(egal);
+//egal.addEventListener("click", function egal(String){
+//    return Function('return' + String)()
+    //screen.textContent= Function('return' + screen.textContent)(); 
+    //eval(screen.textContent);
+//});
+
+//const egal = document.createElement("button");
+//egal.textContent = "=";
+//main.appendChild(egal);
+//egal.addEventListener("click", function egal(String){
+//    return Function('return' + String)()
+    //screen.textContent= Function('return' + screen.textContent)(); 
+    //eval(screen.textContent);
+//});
+
+// Ajout de decimale 
+//const decimal = document.createElement("button");
+//decimal.textContent = ",";
+//main.appendChild(decimal);
+//decimal.addEventListener("click", function(){
+//    screen.textContent = 
 
 
 
