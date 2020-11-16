a474786eef1b2f172c6533477d61aa9fb82b1d6e
Bérengère Carpentier <72983609+carpentierberengere@users.noreply.github.com>
2020-11-16 10:59:55 +0100

    début code

---
+++ calculator.js
@@ -0,0 +1,34 @@
+//const calculator = {
+//    displayValue : '0',
+//    firstOperand : null,
+//    waitingFotSecondOperand : false,
+//    operator: null
+
+const body = document.querySelector("body");
+const screen = document.createElement("div");
+
+const addition = document.createElement("button");
+addition.textContent.Content = "+";
+body.appendChild(addition);
+
+const division = document.createElement("body");
+division.textContent.Content
+
+
+const multiplication = document.createElement("button");
+multiplication.textContent.Content = "*";
+body.appendChild(multiplication);
+
+for(let i = 0; i <= 10; i++) {
+    const button = document.createElement("button");
+    button.textContent=i;
+
+    body.appendChild(button);
+    body.appendChild(button);
+
+    multiplication.addEventListener("click", function(){
+        screen.textContent = "Well Hello There";
+    })
+
+}
+
