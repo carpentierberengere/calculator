5fc06c7b6b3860594e7a6a8a15d282ea58fc7e66
Bérengère Carpentier <72983609+carpentierberengere@users.noreply.github.com>
2020-11-16 12:23:26 +0100

    display changes

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-const body = document.querySelector("body");
+const main = document.querySelector("main");
 
 const screen = document.createElement("div");
 
@@ -7,23 +7,23 @@ for (let i = 0; i <= 9; i++) {
     const button = document.createElement("button");
     button.textContent = i;
   
-    body.appendChild(button);
+    main.appendChild(button);
   
     button.addEventListener("click", function () {
-      ecran.textContent += String(i);
+      screen.textContent += String(i);
     });
   }
 
 const c = document.createElement("button");
 c.textContent = "C";
-body.appendChild(c);
+main.appendChild(c);
 c.addEventListener("click", function(){
     screen.textContent = "";
 });
 
 const egal = document.createElement("button");
 egal.textContent = "=";
-body.appendChild(egal);
+main.appendChild(egal);
 egal.addEventListener("click", function(){
     screen.textContent=eval(screen.textContent);
 });
@@ -33,14 +33,14 @@ const operations= ["+","-","*",":"]
 for (let i=0; i<= operations.length-1;i++) {
     const op = document.createElement("button");
     op.textContent = operations[i];
-    body.appendChild(op);
+    main.appendChild(op);
     op.addEventListener("click", function () {
-        ecran.textContent += operations[i];
+        screen.textContent += operations[i];
     });
 }
 
-body.appendChild(screen);
+main.appendChild(screen);
 screen.style.border = "2px solid black";
-screen.style.height = "150px";
+screen.style.height = "50px";
 
 
